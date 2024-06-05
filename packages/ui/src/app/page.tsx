57cb847dc89209b56b920dc6e5f3c376e4bfc7cc
Ernest Cui <ecuiships@126.com>
2024-06-05 18:02:31 +0800

    chore: configure pagination

--- packages/ui/src/app/page.tsx
+++ packages/ui/src/app/page.tsx
@@ -601,7 +601,9 @@ export default function Home() {
             columns={columns}
             dataSource={displayMessages}
             pagination={{
+              defaultPageSize: 20,
               pageSizeOptions: [10, 20, 30, 50, 100, 200],
+              showSizeChanger: true,
               showQuickJumper: true,
             }}
           />
